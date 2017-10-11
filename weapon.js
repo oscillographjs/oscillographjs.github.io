ad6d2b2d7cf02e127893276c85c0b48d4674d1e4
Шамиль Джамалудинов <shitabrix@gmail.com>
2017-10-12 02:43:09 +0300

    Добавлены новые классы оружия

--- weapon.js
+++ weapon.js
@@ -1,13 +1,21 @@
 const Weapon = Prototype.create();
 Weapon.factory.init = function (instance, args) {
+  createConstProperty(instance, 'name', args.name);
+  createConstProperty(instance, 'model', args.model);
 };
 const Grenade = Prototype.extend(Weapon);
-Grenade.factory.init = function(instance, args) {
-  const {model} = args;
-  createConstProperty(instance, 'model', model);
+Grenade.factory.init = function (instance, args) {
+  Weapon.factory.init(instance, args);
+  let hasBeenUsed = false;
   createConstProperty(instance, 'isReusable', false);
+  instance.use = () => hasBeenUsed = true;
 };
-const FireGrenade = Prototype.extend(Grenade);
-FireGrenade.factory.init = function (instance, args) {
-  Grenade.factory.init(instance, args);
+const Knife = Prototype.extend(Weapon);
+const Riffle = Prototype.extend(Weapon);
+Riffle.factory.init = function (instance, args) {
+  Weapon.factory.init(instance, args);
+  let clipSize = 30;
+  const reloadingTime = 2; //seconds
+  createConstProperty(instance, 'isReusable', true);
+  instance.getClipSize = () => clipSize;
 };
