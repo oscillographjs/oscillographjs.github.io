8e86f50f35664a3c5f45746529c4e2afaefb5511
Шамиль <shitabrix@gmail.com>
2019-04-21 15:41:13 +0300

    удалил старый код

--- main.js
+++
@@ -1,9 +0,0 @@
-const unitOne = Unit.factory.create({
-  name: "John"
-});
-const trooperOne = Trooper.factory.create({
-  name: "Bob",
-});
-const marinOne = MarinTrooper.factory.create({
-  name: "Sam",
-});
